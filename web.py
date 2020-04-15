a9fe50eec94a4ebf61363aae390b38db89e8a03c
gabrielmotroni-prog <60718584+gabrielmotroni-prog@users.noreply.github.com>
2020-04-14 22:20:01 -0300

    Update web.py

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return " Senha Lum123"
+    return " Senha Lum123  ps: Bruna teamo minha gatona"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
